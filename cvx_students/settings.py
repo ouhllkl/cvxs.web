8608fe652eb00440fa492d88005cfef5db0cb432
philip <credom.it@gmail.com>
2023-05-04 00:17:48 +0200

    hosting settings

--- cvx_students/settings.py
+++ cvx_students/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'universities'
+    'universities',
     'allauth',
     'allauth.account',
 ]
